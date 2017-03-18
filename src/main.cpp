7e0f2d86e9bff3cb227b650647d665b322f37536
Mátyás Mustoha <matyas.mustoha@gmail.com>
2017-03-18 20:40:32 +0100

    Tell the QML engine the application's path

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("appDirPath", QGuiApplication::applicationDirPath());
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
