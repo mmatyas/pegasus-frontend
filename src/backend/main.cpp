390e2707b8607622e376fa225381b004d0fc6982
Mátyás Mustoha <matyas.mustoha@gmail.com>
2017-03-21 16:40:46 +0100

    Revert "Tell the QML engine the application's path"
    This reverts commit 7e0f2d86e9bff3cb227b650647d665b322f37536.

--- src/backend/main.cpp
+++ src/backend/main.cpp
@@ -1,13 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QQmlContext>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.rootContext()->setContextProperty("appDirPath", QGuiApplication::applicationDirPath());
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
     return app.exec();
