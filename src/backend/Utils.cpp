666406ccecbf7428279f56dc57667fe5be103430
Mátyás Mustoha <matyas.mustoha@gmail.com>
2017-04-20 18:54:32 +0200

    Added fast POSIX file existence checking

--- src/backend/Utils.cpp
+++ src/backend/Utils.cpp
@@ -4,10 +4,21 @@
 #include <QString>
 #include <QVector>
 
+#ifdef Q_OS_UNIX
+#include <sys/stat.h>
+#endif
+
 
 bool validFile(const QString& path) {
+#ifdef Q_OS_UNIX
+    // fast posix check for unix systems
+    static struct ::stat buffer;
+    return (::stat(path.toUtf8().constData(), &buffer) == 0);
+#else
+    // default Qt fallback
     QFileInfo file(path);
     return file.exists() && file.isFile();
+#endif
 }
 
 void parseStoreInt(const QString &str, int &val) {
