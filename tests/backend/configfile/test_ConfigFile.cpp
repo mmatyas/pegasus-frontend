8fe3b7d6880bf7412f9d3accc8ea8ecfb1102b1c
Mátyás Mustoha <matyas.mustoha@gmail.com>
2017-12-21 20:51:19 +0100

    Added ConfigFile test

--- tests/backend/configfile/test_ConfigFile.cpp
+++ tests/backend/configfile/test_ConfigFile.cpp
@@ -26,6 +26,7 @@ class test_ConfigFile : public QObject {
 private slots:
     void empty();
     void datablob();
+    void file();
 };
 
 void test_ConfigFile::empty()
@@ -44,11 +45,39 @@ void test_ConfigFile::datablob()
     QByteArray buffer(1024, 0x0);
     QTextStream stream(buffer);
 
-    QTest::ignoreMessage(QtWarningMsg, QRegularExpression("`.*?`: line [0-9]+ is invalid, skipped"));
+    QTest::ignoreMessage(QtWarningMsg, QRegularExpression("`.*?`: line 1 is invalid, skipped"));
     config::Config config = config::readStream(stream);
 
     QCOMPARE(config.contains(QString()), true);
     QCOMPARE(config.value(QString()).count(), 0);
+}
+
+void test_ConfigFile::file()
+{
+    QTest::ignoreMessage(QtWarningMsg, QRegularExpression("`:/test.cfg`: line 3 is invalid, skipped"));
+    QTest::ignoreMessage(QtWarningMsg, QRegularExpression("`:/test.cfg`: line 14 is invalid, skipped"));
+    QTest::ignoreMessage(QtWarningMsg, QRegularExpression("`:/test.cfg`: line 15 is invalid, skipped"));
+    QTest::ignoreMessage(QtWarningMsg, QRegularExpression("`:/test.cfg`: line 16 is invalid, skipped"));
+    config::Config config = config::read(":/test.cfg");
+
+    config::Config expected = {
+        {QString(), { {"global", "value"} }},
+        {"sectionname", {
+              {"key1", "val"},
+              {"key2", "val"},
+              {"key with spaces", "val with spaces"},
+              {"option with", "as delimiter"},
+        }},
+        {"sectionname with spaces", {}},
+        {"multilines", {
+             {"multiline1", "hello world!"},
+             {"multiline2", "a line with\nline break"},
+        }},
+        {"arrays", {
+             {"array", QVariantList({"value1", "value2", "value3"})}
+        }},
+    };
+    QCOMPARE(config, expected);
 }
 
 
