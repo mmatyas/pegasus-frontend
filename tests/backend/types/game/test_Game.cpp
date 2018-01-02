7c5f9a9fd35ff166d6ec035615c5d5c93e4908b3
Mátyás Mustoha <matyas.mustoha@gmail.com>
2018-01-02 19:52:38 +0100

    Added tests for the Game

---
+++ tests/backend/types/game/test_Game.cpp
@@ -0,0 +1,93 @@
+// Pegasus Frontend
+// Copyright (C) 2017  Mátyás Mustoha
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+#include <QtTest/QtTest>
+
+#include "types/Game.h"
+
+
+class test_Game : public QObject {
+    Q_OBJECT
+
+private slots:
+    void developers();
+    void publishers();
+    void genres();
+    void release();
+};
+
+void testStrAndList(Types::Game& game,
+                    std::function<void(const QString&)> fn_add,
+                    const char* str_name, const char* list_name)
+{
+    QVERIFY(game.property(str_name).toString().isEmpty());
+    QVERIFY(game.property(list_name).toStringList().isEmpty());
+
+    // empty
+    fn_add({});
+    QVERIFY(game.property(str_name).toString().isEmpty());
+    QVERIFY(game.property(list_name).toStringList().isEmpty());
+
+    // first
+    fn_add("test1");
+    QCOMPARE(game.property(str_name).toString(), QStringLiteral("test1"));
+    QCOMPARE(game.property(list_name).toStringList(), {"test1"});
+
+    // second
+    fn_add("test2");
+    QCOMPARE(game.property(str_name).toString(), QStringLiteral("test1, test2"));
+    QCOMPARE(game.property(list_name).toStringList(), QStringList({"test1", "test2"}));
+
+    // third
+    fn_add("test3");
+    QCOMPARE(game.property(str_name).toString(), QStringLiteral("test1, test2, test3"));
+    QCOMPARE(game.property(list_name).toStringList(), QStringList({"test1", "test2", "test3"}));
+}
+
+void test_Game::developers()
+{
+    Types::Game game({});
+    auto fn = [&game](const QString& val){ game.addDeveloper(val); };
+    testStrAndList(game, fn, "developer", "developerList");
+}
+
+void test_Game::publishers()
+{
+    Types::Game game({});
+    auto fn = [&game](const QString& val){ game.addPublisher(val); };
+    testStrAndList(game, fn, "publisher", "publisherList");
+}
+
+void test_Game::genres()
+{
+    Types::Game game({});
+    auto fn = [&game](const QString& val){ game.addGenre(val); };
+    testStrAndList(game, fn, "genre", "genreList");
+}
+
+void test_Game::release()
+{
+    Types::Game game({});
+    game.setRelease(QDate(1999,1,2));
+    QCOMPARE(game.property("year").toInt(), 1999);
+    QCOMPARE(game.property("month").toInt(), 1);
+    QCOMPARE(game.property("day").toInt(), 2);
+}
+
+
+QTEST_MAIN(test_Game)
+#include "test_Game.moc"
--- tests/backend/types/types.pro
+++ tests/backend/types/types.pro
@@ -3,6 +3,7 @@ TEMPLATE = subdirs
 SUBDIRS += \
     collection \
     collectionlist \
+    game \
     gameassets \
     gamelist \
     localelist \
