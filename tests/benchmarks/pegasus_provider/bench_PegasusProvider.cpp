f04c0b6ddcc5107f5d6d86194ed74638b16dfdfe
Mátyás Mustoha <matyas.mustoha@gmail.com>
2019-01-24 21:25:02 +0100

    Data structure changes: Updated the benchmarks

--- tests/benchmarks/pegasus_provider/bench_PegasusProvider.cpp
+++ tests/benchmarks/pegasus_provider/bench_PegasusProvider.cpp
@@ -1,5 +1,5 @@
 // Pegasus Frontend
-// Copyright (C) 2017-2018  Mátyás Mustoha
+// Copyright (C) 2017-2019  Mátyás Mustoha
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,8 +18,6 @@
 #include <QtTest/QtTest>
 
 #include "providers/pegasus/PegasusProvider.h"
-#include "modeldata/gaming/CollectionData.h"
-#include "modeldata/gaming/GameData.h"
 
 #include <QString>
 
@@ -34,28 +32,23 @@ private slots:
 
 void bench_PegasusProvider::find_in_empty_dir()
 {
-    HashMap<QString, modeldata::Game> games;
-    HashMap<QString, modeldata::Collection> collections;
-    HashMap<QString, std::vector<QString>> collection_childs;
-
+    providers::SearchContext sctx;
     providers::pegasus::PegasusProvider provider({QStringLiteral(":/empty")});
 
     QBENCHMARK {
-        provider.findLists(games, collections, collection_childs);
+        QTest::ignoreMessage(QtWarningMsg, "Collections: No metadata file found in `:/empty`, directory ignored");
+        provider.findLists(sctx);
     }
 }
 
 void bench_PegasusProvider::find_in_filled_dir()
 {
-    HashMap<QString, modeldata::Game> games;
-    HashMap<QString, modeldata::Collection> collections;
-    HashMap<QString, std::vector<QString>> collection_childs;
-
+    providers::SearchContext sctx;
     providers::pegasus::PegasusProvider provider({QStringLiteral(":/filled")});
 
     QBENCHMARK {
         QTest::ignoreMessage(QtInfoMsg, "Collections: found `:/filled/collections.txt`");
-        provider.findLists(games, collections, collection_childs);
+        provider.findLists(sctx);
     }
 }
 
