24668bb13d8d40cb7283bad5dc6c92bd3b3b865b
Mátyás Mustoha <matyas.mustoha@gmail.com>
2019-03-26 19:56:04 +0100

    Some refactoring in the Metadata asset file searching

--- src/backend/providers/pegasus/PegasusMedia.cpp
+++ src/backend/providers/pegasus/PegasusMedia.cpp
@@ -35,55 +35,63 @@ AssetType detect_asset_type(const QString& basename, const QString& ext)
 
     return AssetType::UNKNOWN;
 }
+
+HashMap<QString, size_t> create_lookup_map(const HashMap<size_t, modeldata::Game>& games)
+{
+    HashMap<QString, size_t> out;
+
+    for (const auto& game_entry : games) {
+        const size_t game_id = game_entry.first;
+        const modeldata::Game& game = game_entry.second;
+
+        for (const modeldata::GameFile& gf_entry : game.files) {
+            const QFileInfo& fi = gf_entry.fileinfo;
+
+            QString extless_path = fi.canonicalPath() % QChar('/') % fi.completeBaseName();
+            out.emplace(std::move(extless_path), game_id);
+
+            // NOTE: the files are not necessarily in the same directory
+            QString title_path = fi.canonicalPath() % QChar('/') % game.title;
+            out.emplace(std::move(title_path), game_id);
+        }
+    }
+
+    return out;
+}
 } // namespace
 
 
 namespace providers {
 namespace pegasus {
 
-void find_assets(const std::vector<QString>& dir_list, HashMap<size_t, modeldata::Game>& games)
+void find_assets(const std::vector<QString>& all_dirs, HashMap<size_t, modeldata::Game>& games)
 {
-    // shortpath: canonical path to dir + extensionless filename
-    HashMap<QString, modeldata::Game* const> games_by_shortpath;
-    games_by_shortpath.reserve(games.size());
-    for (auto& entry : games) {
-        modeldata::Game& game = entry.second;
-
-        for (const modeldata::GameFile& gf_entry : qAsConst(game.files)) {
-            const QFileInfo& fi = gf_entry.fileinfo;
-
-            QString shortpath = fi.canonicalPath() % '/' % fi.completeBaseName();
-            games_by_shortpath.emplace(std::move(shortpath), &game);
-
-            // TODO: could be optimized
-            QString namepath = fi.canonicalPath() % '/' % game.title;
-            games_by_shortpath.emplace(std::move(namepath), &game);
-        }
-    }
-
-
     constexpr auto dir_filters = QDir::Files | QDir::Readable | QDir::NoDotAndDotDot;
     constexpr auto dir_flags = QDirIterator::Subdirectories | QDirIterator::FollowSymlinks;
     constexpr int media_len = 6; // len of `/media`
 
-    for (const QString& dir_base : dir_list) {
-        const QString media_dir = dir_base + QStringLiteral("/media");
+    const HashMap<QString, size_t> lookup_map = create_lookup_map(games);
+
+    for (const QString& dir_base : all_dirs) {
+        const QString media_dir = dir_base + QLatin1String("/media");
 
         QDirIterator dir_it(media_dir, dir_filters, dir_flags);
         while (dir_it.hasNext()) {
             dir_it.next();
             const QFileInfo fileinfo = dir_it.fileInfo();
 
-            const QString shortpath = fileinfo.canonicalPath().remove(dir_base.length(), media_len);
-            if (!games_by_shortpath.count(shortpath))
+            const QString lookup_key = fileinfo.canonicalPath().remove(dir_base.length(), media_len);
+            const auto lookup_it = lookup_map.find(lookup_key);
+            if (lookup_it == lookup_map.cend())
                 continue;
 
             const AssetType asset_type = detect_asset_type(fileinfo.completeBaseName(), fileinfo.suffix());
             if (asset_type == AssetType::UNKNOWN)
                 continue;
 
-            modeldata::Game* const game = games_by_shortpath[shortpath];
-            game->assets.addFileMaybe(asset_type, dir_it.filePath());
+            const size_t game_id = lookup_it->second;
+            modeldata::Game& game = games.at(game_id);
+            game.assets.addFileMaybe(asset_type, dir_it.filePath());
         }
     }
 }
