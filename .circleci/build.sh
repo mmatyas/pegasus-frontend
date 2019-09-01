b7e56991bd0cbbf12cc7116687eae32dff50055e
Mátyás Mustoha <matyas.mustoha@gmail.com>
2019-09-01 17:34:44 +0200

    Build using SDL2 on Circle CI Windows

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -9,7 +9,14 @@ if [[ ! -d "$QT_DIR" ]]; then
   QT_DIR=/opt/${QT_VERSION}_${QT_TARGET}
 fi
 
+BUILDOPTS=""
+if [ -n "${SDL_ROOT:-}" ]; then
+  BUILDOPTS+=" USE_SDL_GAMEPAD=1"
+  BUILDOPTS+=" SDL_LIBS='-L${SDL_ROOT}/lib/ ${SDL_LIBS:-}'"
+  BUILDOPTS+=" SDL_INCLUDES='${SDL_ROOT}/include/SDL2/'"
+fi
+
 ${QT_DIR}/bin/qmake --version
-${QT_DIR}/bin/qmake .
+eval ${QT_DIR}/bin/qmake . ${BUILDOPTS:-}
 make
 make install INSTALL_ROOT=${PWD}/installdir
