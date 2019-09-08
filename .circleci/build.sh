efd4dd9d50344ffa8268a50fcf1270ca68e07040
Mátyás Mustoha <matyas.mustoha@gmail.com>
2019-09-08 17:45:15 +0200

    Store Circle CI build options in the config file

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -9,13 +9,6 @@ if [[ ! -d "$QT_DIR" ]]; then
   QT_DIR=/opt/${QT_VERSION}_${QT_TARGET}
 fi
 
-BUILDOPTS=""
-if [ -n "${SDL_ROOT:-}" ]; then
-  BUILDOPTS+=" USE_SDL_GAMEPAD=1"
-  BUILDOPTS+=" SDL_LIBS='-L${SDL_ROOT}/lib/ ${SDL_LIBS:-}'"
-  BUILDOPTS+=" SDL_INCLUDES='${SDL_ROOT}/include/SDL2/'"
-fi
-
 ${QT_DIR}/bin/qmake --version
 eval ${QT_DIR}/bin/qmake . ${BUILDOPTS:-}
 make
