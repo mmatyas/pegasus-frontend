59031c71a5cd0dba9f322e39ad0b27c33541a872
Mátyás Mustoha <matyas.mustoha@gmail.com>
2021-01-14 22:07:33 +0100

    Use the patched PNG headers on the CIs

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -12,3 +12,4 @@ mkdir dist
 
 TOOLS_URL=https://github.com/mmatyas/pegasus-frontend/releases/download/alpha1
 curl -L ${TOOLS_URL}/${QT_VERSION}_${QT_TARGET}.tar.xz | tar xJf - -C /opt/
+curl -L ${TOOLS_URL}/apng_patched_headers.tar.xz | tar xJf - -C /opt/${QT_VERSION}_${QT_TARGET}/include/
