17549e4edd31d45a4bb7bd5184e038701df8d90f
Mátyás Mustoha <matyas.mustoha@gmail.com>
2021-01-17 17:41:59 +0100

    Moved Bintray deployment to Circle CI

---
+++ .circleci/release_bintray.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+set -o errexit
+set -o nounset
+set +o xtrace # !
+
+shopt -s nullglob
+
+if [[ "$CIRCLE_BRANCH" != "master" ]]; then
+  echo "Skipping deployment for pull requests"
+  exit 0
+fi
+
+
+GIT_TAG=$(git describe --always --abbrev=0)
+
+
+for file in "dist-x11-static/*.deb"; do
+    echo "Uploading '${file}' to Bintray"
+
+    filename=$(basename "$file")
+    curl \
+        -T "${file}" \
+        -H "X-Bintray-Package: pegasus-frontend" \
+        -H "X-Bintray-Version: ${GIT_TAG}" \
+        -H "X-Bintray-Publish: 1" \
+        -H "X-Bintray-Debian-Distribution: testing" \
+        -H "X-Bintray-Debian-Component: main" \
+        -H "X-Bintray-Debian-Architecture: amd64" \
+        -u "mmatyas:${BINTRAY_KEY}" \
+        "https://api.bintray.com/content/mmatyas/deb/pool/main/p/pegasus-frontend/${filename}"
+done
